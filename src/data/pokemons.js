059d4d5dfae0ecdb70220e595ea18567e3454b92
alperonoberto <alperonoberto@gmail.com>
2025-05-18 23:37:16 +0200

    100 first pokemon fetch, pokecard, data compiling

---
+++ src/data/pokemons.js
@@ -0,0 +1,39 @@
+export const pokemonList = []
+
+for(let i = 0; i < 100; i++) {
+    await fetch(`https://pokeapi.co/api/v2/pokemon/${i + 1}`)
+    .then(response => response.json())
+    .then(data => pokemonList.push(data));
+}
+
+export async function pokemonType(type) {
+    const response = await fetch(`https://pokeapi.co/api/v2/type/${type.type.name}`);
+    if (!response.ok) {
+        throw new Error(`Error al obtener pokemons: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+};
+
+
+
+export const typeColours = {
+	normal: '#A8A77A',
+	fire: '#EE8130',
+	water: '#6390F0',
+	electric: '#F7D02C',
+	grass: '#7AC74C',
+	ice: '#96D9D6',
+	fighting: '#C22E28',
+	poison: '#A33EA1',
+	ground: '#E2BF65',
+	flying: '#A98FF3',
+	psychic: '#F95587',
+	bug: '#A6B91A',
+	rock: '#B6A136',
+	ghost: '#735797',
+	dragon: '#6F35FC',
+	dark: '#705746',
+	steel: '#B7B7CE',
+	fairy: '#D685AD',
+};
